da48f743ddcd4f8f52839882df0e62b30e251e29
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-01-19 15:59:55 -0300

    refactor: remove authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { AuthModule } from './authentication/authentication.module';
-import { AuthorizationModule } from './authorization/authorization.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 @Module({
   imports: [
     UsersModule,
-    AuthModule,
     MailerModule.forRoot({
       transport: {
         logger: true,
@@ -25,7 +22,6 @@ dotenv.config();
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthorizationModule,
   ],
   controllers: [],
   providers: [],
