181175b4e66f30932b036d601269490e50566ebe
Luiz Pires - Ranzinza <kw13code@gmail.com>
2024-07-04 14:20:47 -0300

    chore: change lib

--- src/common/hashing/domain/services/hashing.service.ts
+++ src/common/hashing/domain/services/hashing.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { IHashingService } from './interfaces/hashing.service.interface';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
+
 @Injectable()
 export class HashingService implements IHashingService {
   public async hashingPassword(
     password: string,
     salt: number,
   ): Promise<string> {
-    return bcrypt.hash(password, salt);
+    try {
+      const saltRounds = bcrypt.genSaltSync(salt);
+      return bcrypt.hashSync(password, saltRounds);
+    } catch (error) {
+      throw new Error('Failed to hash password');
+    }
   }
 }
